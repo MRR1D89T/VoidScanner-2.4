223235907c9d260d55bbb562cf79f95879f212b5
MR. S13xVoid <125761438+MRR1D89T@users.noreply.github.com>
2025-07-25 15:36:11 +0700

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,1 +1,20 @@
-
+def show_banner():
+    banner = r"""
+\033[1;32m
+░█▄█░█▀█░█▀▄░░░█▀█░█▀▄░█▀▀░█▀█░█▄█
+░█░█░█▀█░█░█░░░█░█░█▀▄░█▀▀░█▀█░█░█
+░▀░▀░▀░▀░▀▀░░░░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀
+\033[1;35m
+     ┌─[ VOIDSCANNER PRO v2.4 - DEEP RECON SUITE ]
+     │   Author : MR.S13xVoid
+     │   Codename: AETHER76 | Operation: ShadowSweep
+     │
+     ├─ Modules :
+     │   [+] SQLi Scanner     [+] XSS Injector
+     │   [+] Admin Finder     [+] Auto Exploiter
+     │   [+] Log Grabber      [+] Header Analyzer
+     │   [+] Nuclei Plugin    [+] Parser & Filter
+     │
+     └─ [ Underground Recon Framework for GhostOps ]
+\033[0m"""
+    print(banner)
